65eb775d18b72477ec4620942b132c539914625d
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-05-27 10:56:42 +0200

    fix names for other techs in cli

--- katoste/cli.py
+++ katoste/cli.py
@@ -38,7 +38,7 @@ def get_index_parser():
         "--flavor",
         type=str,
         default="openst",
-        choices=['openst', 'stereoseq', 'slideseqv2', 'visium', 'seqscopev1', 'visiumhd', '*.yaml'], # TODO: formatting of the .yaml is acceptable, or move validation to the function
+        choices=['openst', 'stereo_seq', 'slide_seq', 'visium', 'seq_scope_v1', '*.yaml'], # TODO: formatting of the .yaml is acceptable, or move validation to the function
         help="""Spatial transcriptomics technology. 
         These are default configurations to read from the paired FASTQ (or BAM) files.
         Other configurations can be provided as a properly formatted `.yaml` file - see
