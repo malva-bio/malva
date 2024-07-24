b6719a30b24d38805c6fc89d03dce458df98f4fd
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-07-24 20:42:37 +0200

    updated to support unordered_dense

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import numpy
-
 from distutils.command.build_ext import build_ext
+from setuptools import Extension, setup
+
+unordered_dense_include = "./include"
 
 class BuildExt(build_ext):
     def build_extensions(self):
@@ -9,13 +11,57 @@ class BuildExt(build_ext):
         except Exception:
             pass
 
+extensions = [
+    Extension(
+        "katoste.kmer_processing",
+        ["katoste/kmer_processing.pyx"],
+        include_dirs=[unordered_dense_include, numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+    Extension(
+        "katoste.fastq_processing",
+        ["katoste/fastq_processing.pyx"],
+        include_dirs=[unordered_dense_include, numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+    Extension(
+        "katoste.reader",
+        ["katoste/reader.pyx"],
+        include_dirs=[unordered_dense_include, numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+    Extension(
+        "katoste.faster_classes",
+        ["katoste/faster_classes.pyx"],
+        include_dirs=[unordered_dense_include, numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+    Extension(
+        "katoste.fast_map",
+        ["katoste/fast_map.pyx"],
+        include_dirs=[unordered_dense_include, numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+]
 
 def build(setup_kwargs):
     from Cython.Build import cythonize
     setup_kwargs.update(
         dict(
             cmdclass=dict(build_ext=BuildExt),
-            ext_modules=cythonize(["katoste/kmer_processing.pyx", "katoste/fastq_processing.pyx", "katoste/reader.pyx", "katoste/faster_classes.pyx"]),
-            include_dirs=[numpy.get_include()],
+            ext_modules=cythonize(extensions),
         )
+    )
+
+if __name__ == "__main__":
+    setup(
+        name="katoste",
+        # Add other necessary setup parameters
+        ext_modules=extensions,
+        cmdclass=dict(build_ext=BuildExt),
     )
