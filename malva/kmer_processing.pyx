c75c70d660e7bbda2698201b850f8c05cc022ec1
Daniel León Periñán <daniel@ilerod.com>
2024-07-27 10:49:39 +0200

    cleanup kmer_processing function

--- malva/kmer_processing.pyx
+++ malva/kmer_processing.pyx
@@ -40,11 +40,12 @@ cdef inline uint64_t _internal_encode_kmer(str kmer):
 
 def get_kmers_numeric(str string, int k, remove_noncomplex=False):
     """Get a list of numerically encoded non-overlapping k-mers from a DNA string."""
-    cdef int n = len(string)
-    cdef int i
-    cdef str kmer, prev_kmer
-    cdef uint64_t encoded_kmer
-    kmers = []
+    cdef:
+        int n = len(string)
+        int i
+        str kmer, prev_kmer
+        uint64_t encoded_kmer
+        list kmers = []
 
     for i in range(0, n, k):
         kmer = string[i:i+k]
@@ -59,29 +60,5 @@ def get_kmers_numeric(str string, int k, remove_noncomplex=False):
     
         encoded_kmer = _internal_encode_kmer(kmer)
         kmers.append(encoded_kmer)
-
-    return kmers
-
-def get_overlapping_kmers_numeric(str string, int k):
-    """Get a list of numerically encoded non-overlapping k-mers from a DNA string."""
-    cdef int n = len(string)
-    cdef int i
-    cdef str kmer, prev_kmer
-    cdef uint64_t encoded_kmer
-
-    if k > 16:
-        kmers = array.array('Q')
-    else:
-        kmers = array.array('I')
-
-    for i in range(0, n):
-        kmer = string[i:i+k]
-        if len(kmer) < k:
-            # Take overlapping nucleotides from the previous k-mer
-            prev_kmer = string[max(0, i-k):i]
-            kmer = prev_kmer[-(k-len(kmer)):] + kmer
-        else:
-            encoded_kmer = _internal_encode_kmer(kmer)
-            kmers.append(encoded_kmer)
 
     return kmers
